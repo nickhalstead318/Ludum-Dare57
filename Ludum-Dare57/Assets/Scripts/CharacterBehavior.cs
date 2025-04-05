09ee3fbf22e2f7ebacc115aaa1366142396495eb
nickhalstead318 <nickhalstead2001@yahoo.com>
2025-04-05 02:33:31 -0500

    Player can now move
    This commit adds a platform, player movement, debug statements (because we'll be glad for them later), the ability to "reset" the scene, and a dirt material for the ground. Expect future commit messages to be less wordy.

--- Ludum-Dare57/Assets/Scripts/CharacterBehavior.cs
+++ Ludum-Dare57/Assets/Scripts/CharacterBehavior.cs
@@ -2,15 +2,81 @@ using UnityEngine;
 
 public class CharacterBehavior : MonoBehaviour
 {
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpForce = 7f;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
+    private Rigidbody rb;
+    private bool isGrounded;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
+        // Sets up the player RigidBody component
+        rb = GetComponent<Rigidbody>();
+        if (rb == null)
+        {
+            Debug.LogWarning("Player object is missing RigidBody component");
+        }
+
+        // Ensures ground check (feet/bottom of player) transform is set up
+        if (groundCheck == null)
+        {
+            Debug.LogWarning("Player object does not have a ground check transform defined");
+        }
+
+        // Ensures ground layer is defined
+        if (groundCheck == null)
+        {
+            Debug.LogWarning("Player object does not have a ground layer defined");
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+        CheckGrounded();
+        Jump();
+        Reset();
+    }
+
+    private void Move()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        rb.linearVelocity = new Vector3(moveX * moveSpeed, rb.linearVelocity.y, 0f);
+    }
+
+    private void CheckGrounded()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, 0.05f, groundLayer);
+    }
+
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpForce, 0f);
+        }
+    }
+
+    private void Reset()
+    {
+        if (Input.GetKeyDown(KeyCode.R)) {
+            transform.SetPositionAndRotation(new Vector3(0, 5, 0), Quaternion.identity);
+            rb.linearVelocity = new Vector3(0, 0, 0);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (groundCheck == null) return;
+
+        Gizmos.color = Color.green;
+        Vector3 boxSize = new Vector3(0.5f, 0.1f, 0.5f);
+        Gizmos.DrawWireCube(groundCheck.position, boxSize);
     }
 }
