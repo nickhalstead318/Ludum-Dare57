5c78c4d74395bbb086016cb35412624a63da7150
JSchade44 <jschade@udel.edu>
2025-04-05 22:20:42 -0500

    Add Color Changing to Background
    Add color changing to background image.

--- Ludum-Dare57/Assets/Scripts/BackgroundBehavior.cs
+++ Ludum-Dare57/Assets/Scripts/BackgroundBehavior.cs
@@ -6,6 +6,14 @@ public class BackgroundBehavior : MonoBehaviour
     [SerializeField]
     CameraBehavior cameraBehavior;
 
+    private float numSteps = 20;
+    private float step = 0;
+    private Color startColor = new Color(164, 121, 109);
+    private Color endColor = new Color(51, 33, 29);
+
+    private float startHeight = 10;
+    private float endHeight = -10;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -20,9 +28,23 @@ public class BackgroundBehavior : MonoBehaviour
 
     void LateUpdate()
     {
-        if (cameraBehavior.GetTarget() != null)
+        Transform target = cameraBehavior.GetTarget();
+        if (target != null)
         {
-            transform.position = new Vector3(cameraBehavior.GetTarget().position.x, cameraBehavior.GetTarget().position.y, transform.position.z);
+            transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+            GetComponent<SpriteRenderer>().color = CalcBackgroundColor(target);
         }
+    }
+
+    private Color CalcBackgroundColor(Transform target)
+    {
+        step = (target.position.y - startHeight) / endHeight;
+        float currRed = startColor.r + (endColor.r - startColor.r) * step / numSteps;
+        float currGreen = startColor.g + (endColor.g - startColor.g) * step / numSteps;
+        float currBlue = startColor.b + (endColor.b - startColor.b) * step / numSteps;
+
+        //step = (step + 1) % numSteps;
+
+        return new Color(currRed / 255, currGreen / 255, currBlue / 255);
     }
 }
--- Ludum-Dare57/Assets/Scripts/CameraBehavior.cs
+++ Ludum-Dare57/Assets/Scripts/CameraBehavior.cs
@@ -9,8 +9,8 @@ public class CameraBehavior : MonoBehaviour
 
     private float numSteps = 20;
     private float step = 0;
-    private Color startColor = new Color(164, 121, 109);
-    private Color endColor = new Color(51, 33, 29);
+    private Color startColor = new Color(255, 255, 255);
+    private Color endColor = new Color(164, 121, 109);
 
     private float startHeight = 10;
     private float endHeight = -10;
@@ -46,7 +46,6 @@ public class CameraBehavior : MonoBehaviour
     void LateUpdate()
     {
         transform.position = target.position + offset;
-        mainCamera.backgroundColor = CalcBackgroundColor();
     }
 
     public Transform GetTarget()
@@ -60,15 +59,5 @@ public class CameraBehavior : MonoBehaviour
         target = newTarget;
     }
 
-    private Color CalcBackgroundColor()
-    {
-        step = (target.position.y - startHeight) / endHeight;
-        float currRed = startColor.r + (endColor.r - startColor.r) * step / numSteps;
-        float currGreen = startColor.g + (endColor.g - startColor.g) * step / numSteps;
-        float currBlue = startColor.b + (endColor.b - startColor.b) * step / numSteps;
-
-        //step = (step + 1) % numSteps;
-
-        return new Color(currRed / 255, currGreen / 255, currBlue / 255);
-    }
+    
 }
