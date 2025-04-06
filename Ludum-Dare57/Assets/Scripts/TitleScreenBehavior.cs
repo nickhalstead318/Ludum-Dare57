3639ccb5335d3d26f810953d53b32c883971018c
JSchade44 <jschade@udel.edu>
2025-04-06 09:54:41 -0500

    Create Title Screen
    Added a title screen scene with buttons to start and quit the game.

---
+++ Ludum-Dare57/Assets/Scripts/TitleScreenBehavior.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreenBehavior : MonoBehaviour
+{
+    private AudioSource _musicPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _musicPlayer = transform.GetComponent<AudioSource>();
+        _musicPlayer.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
